242f9fc174f96f0ef290954593487c6ac08b2c87
Sofyan OJEER <sofyanojeer@gmail.com>
2022-06-20 09:50:43 +0200

    second commit

---
+++ app/src/main/java/com/example/mythermometer/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.mythermometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v4.app.INotificationSideChannel;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Thermometer thermometer;
+    private float temperature;
+    private Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        thermometer = (Thermometer) findViewById(R.id.thermometer);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        simulateAmbientTemperature();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterAll();
+    }
+
+    private void simulateAmbientTemperature() {
+        timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                temperature = Utils.randInt(-20, 55);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        thermometer.setCurrentTemp(temperature);
+                        getSupportActionBar().setTitle(getString(R.string.app_name) + " : " + temperature);
+                    }
+                });
+            }
+        }, 0, 3500);
+    }
+
+    private void unregisterAll() {
+        timer.cancel();
+    }
+
+}
